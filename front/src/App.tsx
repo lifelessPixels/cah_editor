b4ea45ea12cb5271511a2317052830ba6f8bc75e
Karol Małecki <malecki.karol@outlook.com>
2024-12-29 12:02:59 +0100

    Initial commit

---
+++ front/src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "antd";
+
+const { Title } = Typography;
+
+const App = () => {
+    return ( 
+        <Title level={1}>My Application!</Title> 
+    );
+};
+
+export default App;
